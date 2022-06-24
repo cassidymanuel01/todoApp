6c37b01f7b188c8be8a6e2e9a56a3f3b2976769e
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-06-24 12:20:10 +0200

    https://github.com/cassidymanuel01/todoApp.git

--- code.js
+++ code.js
@@ -1,73 +1,96 @@
+(function() {
+	'use strict';
+	var tasker = {
+		init: function() {
+			this.cacheDom();
+			this.bindEvents();
+			this.evalTasklist();
+		},
+		cacheDom: function() {
+			this.taskInput = document.getElementById("input-task");
+			this.addBtn = document.getElementById("add-task-btn");
+			this.tasklist = document.getElementById("tasks");
+			this.tasklistChildren = this.tasklist.children;
+			this.errorMessage = document.getElementById("error");
+		},
+		bindEvents: function() {
+			this.addBtn.onclick = this.addTask.bind(this);
+			this.taskInput.onkeypress = this.enterKey.bind(this);
+		},
+		evalTasklist: function() {
+			var i, chkBox, delBtn;
+			//BIND CLICK EVENTS TO ELEMENTS
+			for (i = 0; i < this.tasklistChildren.length; i += 1) {
+				//ADD CLICK EVENT TO CHECKBOXES
+				chkBox = this.tasklistChildren[i].getElementsByTagName("input")[0];
+				chkBox.onclick = this.completeTask.bind(this, this.tasklistChildren[i], chkBox);
+				//ADD CLICK EVENT TO DELETE BUTTON
+				delBtn = this.tasklistChildren[i].getElementsByTagName("button")[0];
+				delBtn.onclick = this.delTask.bind(this, i);
+			}
+		},
+		render: function() {
+			var taskLi, taskChkbx, taskVal, taskBtn, taskTrsh;
+			//BUILD HTML
+			taskLi = document.createElement("li");
+			taskLi.setAttribute("class", "task");
+			//CHECKBOX
+			taskChkbx = document.createElement("input");
+			taskChkbx.setAttribute("type", "checkbox");
+			//USER TASK
+			taskVal = document.createTextNode(this.taskInput.value);
+			//DELETE BUTTON
+			taskBtn = document.createElement("button");
+			//TRASH ICON
+			taskTrsh = document.createElement("i");
+			taskTrsh.setAttribute("class", "fa fa-trash");
+			//INSTERT TRASH CAN INTO BUTTON
+			taskBtn.appendChild(taskTrsh);
 
-var btn = document.querySelector('.add');
-var remove = document.querySelector('.draggable');
+			//APPEND ELEMENTS TO TASKLI
+			taskLi.appendChild(taskChkbx);
+			taskLi.appendChild(taskVal);
+			taskLi.appendChild(taskBtn);
 
-function dragStart(e) {
-  this.style.opacity = '0.4';
-  dragSrcEl = this;
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
-};
+			//ADD TASK TO TASK LIST
+			this.tasklist.appendChild(taskLi);
 
-function dragEnter(e) {
-  this.classList.add('over');
-}
+		},
+		completeTask: function(i, chkBox) {
+			if (chkBox.checked) {
+				i.className = "task completed";
+			} else {
+				this.incompleteTask(i);
+			}
+		},
+		incompleteTask: function(i) {
+			i.className = "task";
+		},
+		enterKey: function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				this.addTask();
+			}
+		},
+		addTask: function() {
+			var value = this.taskInput.value;
+			this.errorMessage.style.display = "none";
 
-function dragLeave(e) {
-  e.stopPropagation();
-  this.classList.remove('over');
-}
+			if (value === "") {
+				this.error();
+			} else {
+				this.render();
+				this.taskInput.value = "";
+				this.evalTasklist();
+			}
+		},
+		delTask: function(i) {
+			this.tasklist.children[i].remove();
+			this.evalTasklist();
+		},
+		error: function() {
+			this.errorMessage.style.display = "block";
+		}
+	};
 
-function dragOver(e) {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = 'move';
-  return false;
-}
-
-function dragDrop(e) {
-  if (dragSrcEl != this) {
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-  return false;
-}
-
-function dragEnd(e) {
-  var listItens = document.querySelectorAll('.draggable');
-  [].forEach.call(listItens, function(item) {
-    item.classList.remove('over');
-  });
-  this.style.opacity = '1';
-}
-
-function addEventsDragAndDrop(el) {
-  el.addEventListener('dragstart', dragStart, false);
-  el.addEventListener('dragenter', dragEnter, false);
-  el.addEventListener('dragover', dragOver, false);
-  el.addEventListener('dragleave', dragLeave, false);
-  el.addEventListener('drop', dragDrop, false);
-  el.addEventListener('dragend', dragEnd, false);
-}
-
-var listItens = document.querySelectorAll('.draggable');
-[].forEach.call(listItens, function(item) {
-  addEventsDragAndDrop(item);
-});
-
-function addNewItem() {
-  var newItem = document.querySelector('.input').value;
-  if (newItem != '') {
-    document.querySelector('.input').value = '';
-    var li = document.createElement('li');
-    var attr = document.createAttribute('draggable');
-    var ul = document.querySelector('ul');
-    li.className = 'draggable';
-    attr.value = 'true';
-    li.setAttributeNode(attr);
-    li.appendChild(document.createTextNode(newItem));
-    ul.appendChild(li);
-    addEventsDragAndDrop(li);
-  }
-}
-
-btn.addEventListener('click', addNewItem);
+	tasker.init();
+}());
